fc79997147ac4138eac5cde361b6996b5dc655f1
weindorf <gehgo2486@yahoo.com>
2023-11-02 16:21:34 +0100

    final

--- src/script.js
+++ src/script.js
@@ -1,18 +1,17 @@
-
-
 class Game {
   constructor() {
     this.gameField = document.getElementById("game-field");
     this.winText = document.getElementById("win-text");
-    this.playerRed1 = new Player(10, 10, 100, 100, "image/red.png", 'red');
-    this.playerRed2 = new Player(110, 310, 100, 100, "image/red.png", 'red');
-    this.playerRed3 = new Player(310, 110, 100, 100, "image/red.png", 'red');
-    this.playerRed4 = new Player(410, 410, 100, 100, "image/red.png", 'red');
-    this.playerBlue1 = new Player(110,110,100,100,"image/blue.png", 'blue');
-    this.playerBlue2 = new Player(10,410,100,100,"image/blue.png", 'blue');
-    this.playerBlue3 = new Player(410,10,100,100,"image/blue.png", 'blue');
-    this.playerBlue4 = new Player(310,310,100,100,"image/blue.png", 'blue');
-    this.playerSelected= [];
+    this.playerRed1 = new Player(10, 10, 100, 100, "image/red.png", "red");
+    this.playerRed2 = new Player(110, 310, 100, 100, "image/red.png", "red");
+    this.playerRed3 = new Player(310, 110, 100, 100, "image/red.png", "red");
+    this.playerRed4 = new Player(410, 410, 100, 100, "image/red.png", "red");
+    this.playerBlue1 = new Player(110, 110, 100, 100, "image/blue.png", "blue");
+    this.playerBlue2 = new Player(10, 410, 100, 100, "image/blue.png", "blue");
+    this.playerBlue3 = new Player(410, 10, 100, 100, "image/blue.png", "blue");
+    this.playerBlue4 = new Player(310, 310, 100, 100, "image/blue.png", "blue");
+    this.gameIsPaused = false;
+    this.playerSelected = [];
     this.players = [
       this.playerBlue1,
       this.playerBlue2,
@@ -23,19 +22,29 @@ class Game {
       this.playerRed3,
       this.playerRed4,
     ];
-   
-  
-    this.players.forEach(player => {
-        player.addEventListener('click', (event) => {
-          this.playerSelected.unshift(player);
-          this.playerSelected.splice(1, 1);
-          console.log('Clicked player:', player);
-          console.log(this.playerSelected);
-        });
-        player.allPlayers = this.players;
-      });
-    
+    this.playersRed = [
+      this.playerRed1,
+      this.playerRed2,
+      this.playerRed3,
+      this.playerRed4,
+    ];
+    this.playersBlue = [
+        this.playerBlue1,
+        this.playerBlue2,
+        this.playerBlue3,
+        this.playerBlue4,
+    ];
 
+    this.players.forEach((player) => {
+      player.addEventListener("click", (event) => {
+        this.playerSelected.unshift(player);
+        this.playerSelected.splice(1, 1);
+        console.log("Clicked player:", player);
+        console.log("player", this.playerSelected);
+        console.log("red", this.playersRed);
+      });
+      player.allPlayers = this.players;
+    });
   }
 
   start() {
@@ -43,56 +52,88 @@ class Game {
   }
 
   gameLoop() {
+
+    if(this.gameIsPaused) {
+        return;
+    }
     this.update();
-    
+
     window.requestAnimationFrame(() => this.gameLoop());
   }
   update() {
     if (this.playerSelected.length > 0) {
-        this.playerSelected[0].move();
-        this.checkWinCondition();
-       
+      this.playerSelected[0].move();
+      this.checkWinConditionRed();
+      this.checkWinConditionBlue();
     }
   }
 
-  checkWinCondition() {
-    const playerColor = this.playerSelected[0].color;
-    let arr = [];
-    // Check for a horizontal line
-    let horizontalCount = 0;
-    for (const player of this.players) {
-      if (player.color === 'red' && player.top === this.playerSelected[0].top) {
-        arr.push(player);
-        //horizontalCount++;
-        console.log(arr);
-      }
-    }
-     /*
-    // Check for a vertical line
-    let verticalCount = 0;
-    for (const player of this.players) {
-      if (player.color === playerColor && player.left === this.playerSelected[0].left) {
-        verticalCount++;
-        console.log(verticalCount);
-      }
-    }
+  checkWinConditionRed() {
+    // console.log(this.playersRed);
+    const result = [];
+    const resultV = [];
 
-    if (horizontalCount >= 4 || verticalCount >= 4) {
-        
-      let h1 = document.createElement("h1");
-      h1.innerHTML = "wins wins";
-      this.winText.appendChild(h1);
+    console.log("20", result);
+    console.log("30",resultV);
+
+    const allEqual = (arr) => arr.every((v) => v === arr[0]);
+    if (result.length <= this.playersRed.length) {
+      this.playersRed.forEach((player) => {
+        result.push(player.top);
+      });
+      if(resultV.length <= this.playersRed.length) {
+        this.playersRed.forEach((el) => {
+            resultV.push(el.left);
+        });
+      }
+      
+      
+      const isAllEqual = allEqual(result);
+      const isLeftE = allEqual(resultV);
+
+
+      if(isAllEqual || isLeftE) {
+        this.gameIsPaused = true;
+        const h1 = document.createElement("h1");
+        h1.innerHTML = "RED WIN'S";
+        this.winText.appendChild(h1);
+      }
     }
-    */
-  }
 }
 
+    checkWinConditionBlue() {
+        const res = [];
+        const resV = [];
 
+    const allEqua = (arr) => arr.every((v) => v === arr[0]);
+    if (res.length <= this.playersBlue.length) {
+      this.playersBlue.forEach((player) => {
+        res.push(player.top);
+      });
+     if(resV.length <= this.playersBlue.length) {
+        this.playersBlue.forEach((e) => {
+            resV.push(e.left);
+        });
+     }
+
+      const isAllEqua = allEqua(res);
+      const isEqLeft = allEqua(resV);
+      
+      if(isAllEqua || isEqLeft) {
+        this.gameIsPaused = true;
+        const h1 = document.createElement("h1");
+        h1.innerHTML = "BLUE WIN'S";
+        this.winText.appendChild(h1);
+      }
+    }
+};
+  
  
+
   
-  
-  
-  
+
+
+}
 
 class Player {
   constructor(left, top, width, height, imgSrc, allPlayers, color) {
@@ -113,19 +154,12 @@ class Player {
     this.gameField.appendChild(this.element);
     this.allPlayers = allPlayers;
     this.color = color;
-    
-    
-
-    
   }
   addEventListener(eventType, callback) {
     this.element.addEventListener(eventType, callback);
   }
 
-
   move() {
-   
-    
     this.left += this.directionX;
     this.top += this.directionY;
     this.updatePosition();
@@ -151,54 +185,47 @@ class Player {
       this.top = maxTop;
     }
 
-    if(this.checkCollisions()) {
-        this.left = prevLeft;
-        this.top = prevTop;
+    if (this.checkCollisions()) {
+      this.left = prevLeft;
+      this.top = prevTop;
     }
-    
 
-
-    
-    this.updatePosition();   
-    
-}
-
-  
-  updatePosition() {
-  
-    this.element.style.left = `${this.left}px`;
-    this.element.style.top = `${this.top}px`;
-  
-    
+    this.updatePosition();
   }
 
+  updatePosition() {
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+  }
 
-
-
-  
   checkCollisions() {
     for (const player of this.allPlayers) {
       if (player !== this) {
-        
         if (
           this.left < player.left + player.width &&
           this.left + this.width > player.left &&
           this.top < player.top + player.height &&
           this.top + this.height > player.top
         ) {
-         
           console.log("detected");
           return true;
-          
         }
       }
     }
     return false;
   }
 }
+const startScreen = document.getElementById("start-screen");
+const gameScreen = document.getElementById("game-screen");
+const startBtn = document.getElementById("start-button");
+startBtn.addEventListener("click", () => {
+  (startScreen.style.display = "none"),
+    (gameScreen.style.display = "inline-block");
+});
 
 window.onload = function () {
   const game = new Game();
+
   const refreshBtn = document.getElementById("refresh-btn");
   refreshBtn.addEventListener("click", () => location.reload());
   game.start();
@@ -211,55 +238,58 @@ window.onload = function () {
       "ArrowRight",
       "ArrowDown",
     ];
-  
+
     if (possibleKeystrokes.includes(key)) {
       event.preventDefault();
-  
-     
+
       const player = game.playerSelected[0];
       const prevLeft = player.left;
       const prevTop = player.top;
-  
-        let newX = player.left;
-        let newY = player.top;
-  
+
+      let newX = player.left;
+      let newY = player.top;
+
       switch (key) {
         case "ArrowLeft":
           newX -= 100;
+
           break;
         case "ArrowUp":
           newY -= 100;
+
           break;
         case "ArrowRight":
           newX += 100;
+
           break;
         case "ArrowDown":
           newY += 100;
+
           break;
       }
-  
+
       // Check if the new position is within the game boundaries
       const minLeft = 10;
       const minTop = 10;
       const maxLeft = game.gameField.offsetWidth - player.width - 10;
       const maxTop = game.gameField.offsetHeight - player.height - 10;
-  
+
       if (newX < minLeft) {
         newX = minLeft;
       }
-  
+
       if (newY < minTop) {
         newY = minTop;
       }
-  
+
       if (newX > maxLeft) {
         newX = maxLeft;
       }
-  
+
       if (newY > maxTop) {
         newY = maxTop;
       }
-  
+
       // Update the player's position
       player.left = newX;
       player.top = newY;
@@ -269,14 +299,9 @@ window.onload = function () {
         player.top = prevTop;
       }
 
-
       player.updatePosition();
-      
     }
   }
-
-
-
 
   function handleKeyup(event) {
     const key = event.key;
@@ -306,10 +331,7 @@ window.onload = function () {
       }
     }
   }
-  
 
   window.addEventListener("keyup", handleKeyup);
   window.addEventListener("keydown", handleKeydown);
-
-  
 };
